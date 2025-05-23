e9665adcbac76edc3be1780a32bc9d9416bcab22
ibrahimkamal <ibrahem.kamal70@gmail.om>
2025-05-23 18:01:45 +0200

    feat(admin): add admin authentication and migration
    - Create a new migration file `2025_05_23_155852_create_admins_table.php` for the `admins` table
    - Define an `Admin` model extending `Authenticatable` for admin user management
    - Update `config/auth.php` to include admin authentication configuration
    - Add `laravel/sanctum` as a dependency for API token authentication
    - Modify `bootstrap/app.php` to include API routes

---
+++ app/Models/Admin.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Admin extends Authenticatable
+{
+    use HasApiTokens;
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+}
